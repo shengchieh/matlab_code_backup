265b6a55a551f7562c2f96f3afc827020dd43e80
sheng chieh <jack81121@gmail.com>
2016-10-27 12:43:13 +0800

    10

--- SHEET.m
+++ SHEET.m
@@ -1,29 +1,28 @@
-clear all;
-clc;
+clear all; close all ; clc;
 
-N = 50;
+N = 100;
 x = linspace(5,-5,N);
 y = x;
-[X,Y] = meshgrid(x);
+[X,Y] = meshgrid(x,y);
 F1 = 0*X;
 F2 = 0*X;
 
 Be = 1;
-THEe = pi/2;
-Bex = Be*sin(THEe);
-Bey = Be*cos(THEe);
+THEe = 0;
+Bex = Be*cos(THEe);
+Bey = Be*sin(THEe);
 A = 1;
 
 for i = 1:N
     for j = 1:N
-        R = X(i,j)*X(i,j) +  Y(i,j)*Y(i,j);
+        R = sqrt(X(i,j)*X(i,j) +  Y(i,j)*Y(i,j));
         THE = atan2(X(i,j),Y(i,j));
         
-        Bx = Bex + A*3*sin(THE)*cos(THE)/(R^3);
-        By = Bey + A*(3*cos(THE)*cos(THE) - 1)/(R^3);
+        Bx = Bex + A*3*cos(THE)*sin(THE) / (R^3);
+        By = Bey + A*(3*cos(THE)*cos(THE) - 1) / (R^3);
         
-        F1(i,j) = atan2(Bx,By);
-        F2(i,j) = sqrt(Bx^2+ By^2);
+        F1(i,j) = atan2(By,Bx);
+        F2(i,j) = sqrt(Bx^2 + By^2);
         if F2(i,j) > 2
             F2(i,j) = 2;
         end
@@ -31,15 +30,12 @@ for i = 1:N
 end
 
 Plate = 0*F1;
+Plate = Plate + THEe;
 
-figure(1)
-surf(X,Y,F1);
-hold on
-surf(X,Y,Plate);
+figure(1);
+surfc(X,Y,F1);
+hold on;
+surfc(X,Y,Plate);
 
-figure(2)
-surf(X,Y,F2);
-hold on
-surf(X,Y,Plate);
-
-hold off
+figure(2);
+surfc(X,Y,F2);
